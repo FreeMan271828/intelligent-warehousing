667016136f43fbf7dcb7bdf751853e0a0522f3f9
FreeMan <1114509193@qq.com>
2024-04-04 15:26:37 +0800

    项目初始化

---
+++ src/test/java/org/iwp/intelligentwarehousing/IntelligentWarehousingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iwp.intelligentwarehousing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentWarehousingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
